5f0daaa3ff2e6c9240b28fe84c3c706b9b28b639
Sergey <santa-klaus88@ya.ru>
2016-11-28 23:44:05 +0200

    Added auxiliary class with createTimeFromSeconds method for further use.

---
+++ SegmentedControl/Utils.h
@@ -0,0 +1,13 @@
+//
+//  Utils.h
+//  SegmentedControl
+//
+//  Created by Serhii Serhiienko on 11/28/16.
+//  Copyright © 2016 Serhii Serhiienko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utils : NSObject
+
+@end
---
+++ SegmentedControl/Utils.m
@@ -0,0 +1,13 @@
+//
+//  Utils.m
+//  SegmentedControl
+//
+//  Created by Serhii Serhiienko on 11/28/16.
+//  Copyright © 2016 Serhii Serhiienko. All rights reserved.
+//
+
+#import "Utils.h"
+
+@implementation Utils
+
+@end
